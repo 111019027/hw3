187661bab4149a668936fd1d7a05d5cfe728099f
111019027 <103715870+111019027@users.noreply.github.com>
2023-03-13 18:49:01 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import 'antd/dist/reset.css';
+import Home from './pages/Home'
+function App() {
+  return (
+    <Home />
+  );
+
+}
+
+export default App;
